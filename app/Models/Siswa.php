66aab2a42f20cc9559009c683aaa1933e27c53fb
fueggyy <luckqmaks@gmail.com>
2022-08-05 12:44:06 +0700

    first commit

---
+++ app/Models/Siswa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    use HasFactory;
+
+    public $timestamps= false;
+    protected $table = 'siswa';
+ 
+    protected $fillable = [
+        'nis',
+        'nisn',
+        'nik',
+        'nama'
+    ];
+}
